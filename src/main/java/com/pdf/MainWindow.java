cff1796986c38dd5b1fe45ae39877c8760d22f13
xiyangxitian <liyan@live.shop.edu.cn>
2019-07-18 14:14:08 +0800

    pdf电子签章4

--- src/main/java/com/pdf/MainWindow.java
+++ src/main/java/com/pdf/MainWindow.java
@@ -21,7 +21,7 @@ import java.security.cert.Certificate;
  * 生成密钥的方法
  * keytool -genkey -v -keystore test.p12 -storetype pkcs12 -alias Myapp -keyalg RSA -keysize 2048 -validity 10000
  *
- * 应用到正式的项目中导入进去了6个jar包才可以了
+ * 应用到正式的项目中导入进去了6个jar包才可以了(是maven项目就不需要导包了)
  * bcpkix-jdk15on-1.49.jar
  * bcprov-jdk15on-1.49.jar
  * itextpdf-5.5.9.jar
