b5edf5033591a73b88a57245e33e0fa08f988ef3
xiyangxitian <liyan@live.shop.edu.cn>
2019-07-18 12:03:02 +0800

    pdf电子签章2

--- src/main/java/com/pdf/MainWindow.java
+++ src/main/java/com/pdf/MainWindow.java
@@ -17,11 +17,15 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 
+/**
+ * 生成密钥的方法
+ * keytool -genkey -v -keystore test.p12 -storetype pkcs12 -alias Myapp -keyalg RSA -keysize 2048 -validity 10000
+ */
 public class MainWindow {
 
     public static final String KEYSTORE = "E:\\my\\aa\\test.p12";
     public static final char[] PASSWORD = "123456".toCharArray();//keystory密码
-    public static final String SRC = "E:\\my\\aa\\bb.pdf";
+    public static final String SRC = "E:\\my\\aa\\gg.pdf";
     public static final String DEST = "E:\\my\\aa\\dest.pdf";
 
     public void sign(String src  //需要签章的pdf文件路径
@@ -47,15 +51,20 @@ public class MainWindow {
         PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
         appearance.setReason(reason);
         appearance.setLocation(location);
+//        appearance.setSignDate(Calendar.getInstance());
         //设置签名的位置，页码，签名域名称，多次追加签名的时候，签名预名称不能一样
         //签名的位置，是图章相对于pdf页面的位置坐标，原点为pdf页面左下角
         //四个参数的分别是，图章左下角x，图章左下角y，图章右上角x，图章右上角y
-        appearance.setVisibleSignature(new Rectangle(200, 200, 300, 300), 1, "sig1");
         //读取图章图片，这个image是itext包的image
-        Image image = Image.getInstance("E:\\my\\pdf\\tool\\gongzhang.png");
+        Image image = Image.getInstance("E:\\my\\aa\\gz.png");
+//        float width = image.getWidth();
+//        float height = image.getHeight();
+//        appearance.setVisibleSignature(new Rectangle(400, 400, 400+width, 400+height), 1, "sig1");
+        appearance.setVisibleSignature(new Rectangle(200, 200, 300, 300), 1, "sig1");
         appearance.setSignatureGraphic(image);
         appearance.setCertificationLevel(PdfSignatureAppearance.NOT_CERTIFIED);
         //设置图章的显示方式，如下选择的是只显示图章（还有其他的模式，可以图章和签名描述一同显示）
+//        appearance.setRenderingMode(PdfSignatureAppearance.RenderingMode.GRAPHIC);
         appearance.setRenderingMode(PdfSignatureAppearance.RenderingMode.GRAPHIC);
 
         // 这里的itext提供了2个用于签名的接口，可以自己实现，后边着重说这个实现
@@ -67,12 +76,12 @@ public class MainWindow {
         MakeSignature.signDetached(appearance, digest, signature, chain, null, null, null, 0, subfilter);
     }
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
             //读取keystore ，获得私钥和证书链
             KeyStore ks = KeyStore.getInstance("PKCS12");
             ks.load(new FileInputStream(KEYSTORE), PASSWORD);
-            String alias = (String)ks.aliases().nextElement();
+            String alias = (String) ks.aliases().nextElement();
             PrivateKey pk = (PrivateKey) ks.getKey(alias, PASSWORD);
             Certificate[] chain = ks.getCertificateChain(alias);
             //new一个上边自定义的方法对象，调用签名方法
